a34f4ef1819d3cb4a99e8a55c88006b2654007c2
TharukaVidurangee <tharukaperera01@gmail.com>
2025-05-31 23:46:44 +0530

    Router & page setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+
+//pages
+import Dashboard from './pages/Dashboard/Dashboard';
+import Create from './pages/Create/Create';
+import Project from './pages/Project/Project';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h3>SyncTrack</h3>
+      <BrowserRouter >
+
+      
+
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+        <Route path="/create">
+          <Create />
+        </Route>
+        <Route path="/projects/:id">
+          <Project />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App
+
+
+
+/* required pages for this project
+
+homepage - dashboard
+login 
+signup
+create
+project details - project 
+
+*/
